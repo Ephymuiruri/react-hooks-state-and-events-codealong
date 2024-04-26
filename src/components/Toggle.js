05ac61e2fd0785599eb1e55edd658bd98cc2def0
Ephy Muiruri Wachira <ephywachira89@gmail.com>
2024-04-26 06:18:10 +0300

    state used appropriately

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React,{ useState } from "react";
 
-function Toggle() {
-  return <button>OFF</button>;
+function Toggl() {
+  const [click,setClick] =useState("OFF")
+  function HandleClick(){
+    if(click === "OFF"){
+      setClick("ON")
+      console.log(click)
+      return(click)
+    } else{
+    setClick("OFF")
+    console.log(click)
+    return(click)
+  }    
+  }
+  return <button onClick={HandleClick}>{click}</button>;
 }
-
+function Toggle() {
+  const [isOn, setIsOn] = useState(false);
+  let click ="OFF"
+  function HandleClick(){
+    setIsOn((isOn)=>!isOn)
+    return(isOn)
+  }
+  return (
+  <button onClick={HandleClick}>{ isOn ? "ON":"OFF"}</button>);
+}
 export default Toggle;
